6c8aafdc79cc4719d0361974bfd74adc79d311d1
gallochri <chri@gallochri.com>
2016-05-15 21:24:02 +0200

    First working version. Putting low pin 5 (D1) will be sent an email address specified.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <Bounce2.h>
 
 // Blynk config
 char auth[] = "x";
@@ -21,20 +22,20 @@ const byte interruptPin = 5;
 
 void emailOnButtonPress()
 {
-  // *** WARNING: You are limited to send ONLY ONE E-MAIL PER MINUTE! ***
-
-  // Let's send an e-mail when you press the button
-  // connected to digital pin 5 on your ESP8266
-
-  int isButtonPressed = !digitalRead(interruptPin); // GPIO5 = D2 Invert state, since button is "Active LOW"
-
-  if (isButtonPressed) // You can write any condition to trigger e-mail sending
+  // Let's send an e-mail when you press the button connected
+  // to digital pin 5 (D1 on nodeMCU) on your ESP8266
+  // Invert state, since button is "Active LOW"
+  int isButtonPressed = !digitalRead(interruptPin);
+  if (isButtonPressed)
   {
-    Serial.println("Button is pressed."); // This can be seen in the Serial Monitor
+    Serial.println("Button is pressed.Try to send email.");
     Blynk.email( usermail, "Subject: You have Mail!", "There's a letter in your postbox!");
     Serial.println("Mail sent.");
   }
 }
+
+// Instantiate a Bounce object to debouncing signal on input pin
+Bounce debouncer = Bounce();
 
 void setup()
 {
@@ -42,19 +43,24 @@ void setup()
   Blynk.begin(auth, ssid, pass, server, port);
 
   while (Blynk.connect() == false) {
-    Serial.println("Blynk.connect is false");
+    Serial.println("Blynk server disconnected");
   }
 
-  Serial.println("Blynk.connect is true.Try to send.");
-  Blynk.email(usermail, "PostESP is online", "Ready.");
-  Serial.println("Mail sent.");
-  // Setting the button
+  //Code for testing connectivity and email settings
+  //Serial.println("Blynk.connect is true.Try to send.");
+  //Blynk.email(usermail, "PostESP is online", "Ready.");
+  //Serial.println("Mail sent.");
+
+  // Setting the sensor pin and Bounce
   pinMode(interruptPin, INPUT_PULLUP);
-  // Attach pin 5 postbox sensor to our handler
-  attachInterrupt(digitalPinToInterrupt(interruptPin), emailOnButtonPress, FALLING);
+  debouncer.attach(interruptPin);
+  debouncer.interval(100); // interval in ms
+
+  // Use attachInterrupt if the sensor havo no bouncing problem
+  //attachInterrupt(digitalPinToInterrupt(interruptPin), emailOnButtonPress, FALLING);
 }
 
-/*/ Keep this flag not to re-sync on every reconnection
+// Keep this flag not to re-sync on every reconnection
 bool isFirstConnect = true;
 // This function will run every time Blynk connection is established
 BLYNK_CONNECTED() {
@@ -65,6 +71,8 @@ BLYNK_CONNECTED() {
     //Blynk.syncVirtual(V0);
     isFirstConnect = false;
   }
+}
+  /*
   // Let's write your hardware uptime to Virtual Pin 2
   int value = millis() / 1000;
   Blynk.virtualWrite(V2, value);
@@ -75,8 +83,13 @@ BLYNK_WRITE(V0)
   Blynk.virtualWrite(V2, value);
 }
 */
-
 void loop()
 {
   Blynk.run();
+  // Update the Bounce instance :
+  debouncer.update();
+  int status = debouncer.read();
+  if (debouncer.fell()){
+    emailOnButtonPress();
+  }
 }
